be23d5d04098969fbbf858786e739a751a35b018
prashulkale <97334196+prashulkale@users.noreply.github.com>
2023-08-30 15:58:21 +0530

    public id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+import { CldUploadButton } from 'next-cloudinary'
 import Image from 'next/image'
+import { CldImage } from 'next-cloudinary';
 
 export default function Home() {
   return (
-   <main>
     
+   <main>
+    <CldUploadButton uploadPreset='xyybqnjw'/>
+    
+<CldImage
+  width="100"
+  height="200"
+  src="oyulcrfs5l9k6jx6hyzo"
+  sizes="100vw"
+  alt="Description of my image"
+/>
    </main>
   )
 }
